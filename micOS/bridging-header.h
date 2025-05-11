e2d008ee42138f2eccdc8df2427b9dfca663b9c2
memoud0 <30944992+memoud0@users.noreply.github.com>
2025-05-10 21:06:24 -0400

    implemented bridge for RNN

---
+++ micOS/bridging-header.h
@@ -0,0 +1,13 @@
+//
+//  bridging-header.h
+//  micOS
+//
+//  Created by Mohamed on 2025-05-10.
+//
+
+#ifndef bridging_header_h
+#define bridging_header_h
+
+#include "rnnoise.h"
+
+#endif /* bridging_header_h */
